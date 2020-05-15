9049c5a4f9fda6a0e3d589582c7ca6bcf2ada8dc
begs <begggsss@protonmail.com>
2020-05-15 17:27:52 +0300

    Added profile file

---
+++ etc/profile.d/vars.sh
@@ -0,0 +1,8 @@
+export VISUAL=kak
+export EDITOR=kak
+export QT_QPA_PLATFORM=wayland
+export SDL_VIDEODRIVER=wayland
+export MOZ_ENABLE_WAYLAND=1
+export DOTNET_ROOT=$HOME/dotnet
+export PATH=$PATH:$HOME/dotnet
+export CLR_OPENSSL_VERSION_OVERRIDE=47
