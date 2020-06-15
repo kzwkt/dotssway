9d14ca3b3efa571cd9f6e6f1a9008b5d8bc0105c
begs <begggsss@protonmail.com>
2020-06-15 18:36:40 +0300

    Change visual and editor to nvim

--- etc/profile.d/vars.sh
+++ etc/profile.d/vars.sh
@@ -1,8 +1,9 @@
-export VISUAL=kak
-export EDITOR=kak
+export VISUAL=nvim
+export EDITOR=nvim
 export QT_QPA_PLATFORM=wayland
 export SDL_VIDEODRIVER=wayland
 export MOZ_ENABLE_WAYLAND=1
 export DOTNET_ROOT=$HOME/dotnet
 export PATH=$PATH:$HOME/dotnet
+export PATH=$PATH:$HOME/.local/bin
 export CLR_OPENSSL_VERSION_OVERRIDE=47
