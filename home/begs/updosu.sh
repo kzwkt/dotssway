7596ac81e80d34ab4f79ae49d64c3a205a4f789b
begs <begggsss@protonmail.com>
2020-07-03 22:21:16 +0300

    Improved osu install/upgrade/build script

--- home/begs/updosu.sh
+++ home/begs/updosu.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+if [ -d "~/osu" ]; then
+    echo "!!! Looks like you're going to build osu! for the first time! Make sure that you have dotnet installed (see https://dotnet.microsoft.com/download/dotnet-core/3.1) and added to PATH (you may use /etc/profile.d/vars.sh from my dotfies as a reference). Also a .desktop file from .local/share/applications may be useful for launching osu."
+    echo ">>> Clonning osu! git repo..."
+    git clone https://github.com/ppy/osu
+fi
 cd osu
+echo ">>> Updating osu! git repo..."
 git pull
-dotnet build -c Release
+echo ">>> Building osu!"
+dotnet build -c Release osu.Desktop
+echo ">>> Done!"
